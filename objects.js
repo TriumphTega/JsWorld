fcbd3de06cd035a9eebd128a08849cf6d818d402
TriumphTega <Beautifulshameless@gmail.com>
2026-01-03 11:02:52 +0100

    Layout change and code clean

--- objects.js
+++
@@ -1,8 +0,0 @@
-let myString = new String
-myString = "Hello"
-console.log(myString.length);
-console.log(myString)
-console.log(myString.toLowerCase());
-
-let myString2 = "Hi there";
-console.log(myString2.length);
